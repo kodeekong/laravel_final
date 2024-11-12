14201a320d8deb2639b1b58fc65353a8a695c6d6
Kodee <kodeekong@yahoo.com>
2024-11-12 08:58:33 -0500

    insert tables

--- database/migrations/2024_11_12_134228_create_rosters_table.php
+++ database/migrations/2024_11_12_134228_create_rosters_table.php
@@ -9,13 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('rosters', function (Blueprint $table) {
             $table->id();
+            $table->date('date');
+            $table->foreignId('supervisor_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('doctor_id')->constrained('users')->onDelete('cascade');
+            $table->json('caregiver_ids')->nullable();
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
--- database/migrations/2024_11_12_134321_create_employee_table.php
+++ database/migrations/2024_11_12_134321_create_employee_table.php
@@ -9,13 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('employee', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('emp_id')->unique();
+            $table->string('role');
+            $table->decimal('salary', 10, 2)->nullable();
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
